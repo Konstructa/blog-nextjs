90a2d7c5d12b2646fe3ca87d24d8934725b4e1ed
Konstructa <elenprojects7@gmail.com>
2023-06-26 14:33:51 -0300

    refactor

--- src/styles/global-styles.ts
+++ src/styles/global-styles.ts
@@ -1,43 +1,5 @@
 import { createGlobalStyle, DefaultTheme  } from 'styled-components';
-
-interface MyTheme extends DefaultTheme {
-  colors: {
-    primary: string;
-    darkText: string;
-    secondary: string;
-    white: string;
-    mediumGray: string;
-  };
-  font: {
-    family: {
-      default: string;
-      secondary: string;
-    };
-    sizes: {
-      xsmall: string;
-      small: string;
-      medium: string;
-      large: string;
-      xlarge: string;
-      xxlarge: string;
-      huge: string;
-      xhuge: string;
-    };
-  };
-  media: {
-    lteMedium: string;
-  };
-  spacings: {
-    xsmall: string;
-    small: string;
-    medium: string;
-    large: string;
-    xlarge: string;
-    xxlarge: string;
-    huge: string;
-    xhuge: string;
-  };
-}
+import { MyTheme } from './MyTheme';
 
 
 export const GlobalStyles = createGlobalStyle<{ theme: MyTheme}>`
