38859f42751b04ad6c985967ff635a87b234a773
Konstructa <elenprojects7@gmail.com>
2023-06-30 10:48:53 -0300

    Feat: Template base

---
+++ src/templates/Base/index.tsx
@@ -0,0 +1,41 @@
+import { Footer } from '../../components/Footer';
+import { GoTop } from '../../components/GoTop';
+import { Header } from '../../components/Header';
+import { Menu } from '../../components/Menu';
+import { SettingsStrapi } from '../../shared-typed/settings-strapi';
+import { ContentContainer, FooterContainer, HeaderContainer, Wrapper } from './styles';
+
+
+export type BaseTemplateProps = {
+  settings: SettingsStrapi;
+  children: React.ReactNode;
+};
+
+
+export const BaseTemplate = ({ settings, children }: BaseTemplateProps) => {
+  return (
+    <Wrapper>
+      <Menu
+        links={settings.menuLink}
+        blogName={settings.blogName}
+        logo={settings.logo.url}
+      />
+
+      <HeaderContainer>
+        <Header
+          blogName={settings.blogName}
+          blogDescription={settings.blogDescription}
+          logo={settings.logo.url}
+        />
+      </HeaderContainer>
+
+      <ContentContainer>{children}</ContentContainer>
+
+      <FooterContainer>
+        <Footer footerHtml={settings.text}/>
+      </FooterContainer>
+
+      <GoTop />
+    </Wrapper>
+  )
+}
---
+++ src/templates/Base/styles.ts
@@ -0,0 +1,21 @@
+import styled from 'styled-components';
+
+export const Wrapper = styled.div``;
+
+export const HeaderContainer = styled.div`
+  margin-top: ${props => props.theme.spacings.xhuge};
+`;
+
+export const ContentContainer = styled.div`
+  max-width: 120rem;
+  width: 100%;
+  margin: 0 auto;
+  margin-bottom: ${props => props.theme.spacings.xhuge};
+`;
+
+export const FooterContainer = styled.div`
+  max-width: 120rem;
+  width: 100%;
+  margin: 0 auto;
+  padding: ${props => props.theme.spacings.large};
+`;
