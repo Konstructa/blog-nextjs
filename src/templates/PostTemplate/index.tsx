6bfc1af780e37cce6acb5bfa03fa99f17273aa30
Konstructa <elenprojects7@gmail.com>
2023-06-30 11:10:51 -0300

    Feat: PostsTemplate

---
+++ src/templates/PostTemplate/index.tsx
@@ -0,0 +1,26 @@
+import { Post } from '../../components/Post';
+import { PostTags } from '../../components/PostTags';
+import { PostStrapi } from '../../shared-typed/post-strapi';
+import { SettingsStrapi } from '../../shared-typed/settings-strapi';
+import { BaseTemplate } from '../Base';
+import { TagsContainer } from './styles';
+
+export type PostTemplateProps = {
+  settings: SettingsStrapi;
+  post: PostStrapi;
+};
+
+
+
+export const PostTemplate = ({ settings, post }: PostTemplateProps) => {
+  console.log(post.tags)
+  return (
+    <BaseTemplate settings={settings}>
+      <Post {...post} />
+
+      <TagsContainer>
+        <PostTags tags={post.tags.tags} />
+      </TagsContainer>
+    </BaseTemplate>
+  );
+};
---
+++ src/templates/PostTemplate/styles.ts
@@ -0,0 +1,5 @@
+import styled from 'styled-components';
+
+export const TagsContainer = styled.div`
+    padding: 0 ${props => props.theme.spacings.large};
+`;
