8c341d00e63248166f19d562538204183e7eeaa0
Konstructa <elenprojects7@gmail.com>
2023-07-03 09:46:47 -0300

    Feat: config graphql

---
+++ src/config/index.ts
@@ -0,0 +1,13 @@
+export default {
+  url: '',
+  siteName: 'Milena Limoeiro',
+  defaultSlug: 'landing-page',
+  graphqlURL: 'http://127.0.0.1:1337/graphql',
+} as ConfigGraphql;
+
+export type ConfigGraphql = {
+  url: string,
+  siteName: string,
+  defaultSlug: string,
+  graphqlURL: string,
+}
